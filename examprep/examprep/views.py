702698b92f0a8763198ac2c02bcec29efa4ba5f1
NavaprettamN <navaprettam.n214@gmail.com>
2024-03-09 12:07:34 +0530

    signin working with csrf

--- examprep/examprep/views.py
+++ examprep/examprep/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.template import RequestContext
 from django.shortcuts import render
 from supabase_py import create_client
+from django.views.decorators.csrf import csrf_protect
 
 supabase_url="https://zhibxigvnqmanyzxvqql.supabase.co"
 supabase_api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoaWJ4aWd2bnFtYW55enh2cXFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5MjY4MDAsImV4cCI6MjAyNTUwMjgwMH0.8nqiWpWSoBNqALzrGL0k8mhL9kLWZyoTav5kRhEZVYs"
@@ -16,11 +18,13 @@ def index(request):
 def signup(request):
     # this is where we verify email -> automatic
     return render(request, "signup.html")
-
+@csrf_protect
 def signin(request):
+    print("check 1")
+    csrfContext = RequestContext(request)
     if request.POST != {}:
         email = request.POST['email']
-        password = request.POST['password']
+        password = request.POST.get('password', False)
         try:
             response = supabase.auth.sign_in(email=email, password=password)
             print(response)
