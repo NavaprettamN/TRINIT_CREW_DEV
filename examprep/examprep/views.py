d71b65cf7ddb66332363c4c35b9f238531fe7f0e
Pranav <pranavv2002@gmail.com>
2024-03-09 16:01:52 +0530

    Signin done at last

--- examprep/examprep/views.py
+++ examprep/examprep/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 from supabase_py import create_client
 from django.views.decorators.csrf import csrf_protect
 
-supabase_url="https://zhibxigvnqmanyzxvqql.supabase.co"
-supabase_api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoaWJ4aWd2bnFtYW55enh2cXFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5MjY4MDAsImV4cCI6MjAyNTUwMjgwMH0.8nqiWpWSoBNqALzrGL0k8mhL9kLWZyoTav5kRhEZVYs"
+supabase_url="https://avkjleinmjywxmspfkck.supabase.co"
+supabase_api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2a2psZWlubWp5d3htc3Bma2NrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5NzIxMDEsImV4cCI6MjAyNTU0ODEwMX0.YiCobkolgIfyGGWSf37K_ECI40KtzwkzA4Kgt9GGACU"
 
 supabase = create_client(supabase_url, supabase_api_key)
 
@@ -16,24 +16,39 @@ def index(request):
     return render(request, "index.html")
 
 def signup(request):
-    # this is where we verify email -> automatic
-    return render(request, "signup.html")
+    if request.POST != {}:
+        username=[]
+        data = supabase.table('user').select('username').execute()
+        for i in data['data']:
+            username.append(i['username'])
+        email = request.POST['email']
+        password = request.POST['password']
+        if email not in username:
+            print(1)
+            data, count = supabase.table('user').insert({"username": email, "password": password}).execute()
+            email, password='',''
+        # this is where we verify email -> automatic
+            return render(request, "dashboard.html", {"username": email})
+        else: return render(request, "signup.html", {"userexists": 1})
+    return render(request, "signup.html", {"userexists": 0})
 @csrf_protect
 def signin(request):
-    print("check 1")
+    # print("check 1")
     csrfContext = RequestContext(request)
     if request.POST != {}:
+        username, passwordarr=[],[]
+        data = supabase.table('user').select('username, password').execute()
+        for i in data["data"]:
+            username.append(i["username"])
+            passwordarr.append(i["password"])
         email = request.POST['email']
-        password = request.POST.get('password', False)
-        try:
-            response = supabase.auth.sign_in(email=email, password=password)
-            print(response)
+        password = request.POST['password']
+        if (email not in username and  password in passwordarr) or (email in username and password not in passwordarr):
+            isAuth=False
+            return render(request, "signin.html", {"error": "Email or Password Error"})
+        if email in username and username.index(email)==passwordarr.index(password):
             isAuth = True
             return render(request, 'dashboard.html', {'isAuth' : isAuth})
-        except:
-            print("failed")
-            isAuth = False
-            return render(request, "signin.html")
     isAuth = False
     return render(request, "signin.html")
 
